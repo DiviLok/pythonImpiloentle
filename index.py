4242044ed960becb8c117d1b0c37a86266d88d21
Divya Jagannatha <dija0002@student.hv.se>
2022-12-28 00:15:21 +0100

    dynamically load videos, added videos into appropriate files and added proper paths in upload function in index.py, changes css for list video page

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, jsonify
 import hashlib
 import os
 # import shutil
@@ -23,31 +23,46 @@ app.config['SECURET_KEY'] = 'supersecretkey'
 # app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 # for upload video
 
-
+# render homepage
 @app.route('/')
 def index():
     return render_template('home.html', loggedin=session.get('status', "false"))
 
-
+# render category page
 @app.route('/videos')
 def catergories():
     return render_template('categories.html', loggedin=session.get('status', "false"))
 
+# dynamically fetch videos based on category
 
-@app.route('/nutrition')
-def nutrition():
-    return render_template('nutrition.html', loggedin=session.get('status', "false"))
+def get_videos(category):
+  base_dir = "/static/videos"
+  category_dir = os.path.join(base_dir,category)
+  videos = os.listdir(os.path.abspath(".") + "/" + category_dir)
+  return videos
+  # return render_template('nutrition.html', loggedin=session.get('status', "false"))
 
 
-@app.route('/newMothers')
-def newMothers():
-    return render_template('newMothers.html', loggedin=session.get('status', "false"))
 
+# display the returned listed videos from get_videos
+@app.route("/videos/<category>")
+def show_videos(category):
+  list_of_videos = get_videos(category)
+  videos = create_video_dictionary(list_of_videos)
+ # print(videos)
+  return render_template("videos.html", videos=videos,loggedin=session.get('status', "false"))
 
-@app.route('/prgnancyvideos')
-def prgnancyvideos():
-    return render_template('prgnancyvideos.html', loggedin=session.get('status', "false"))
-
+  # creating dictionary videos for the list of videos 
+def create_video_dictionary(videos):
+  result = []
+  for video in videos:
+    result.append(
+      {
+        "src": "/static/videos/" + video,
+        "title": video.split(".")[0] # split title of the video
+      }
+    )
+  return result
 
 @app.route('/about')
 def about():
@@ -79,15 +94,14 @@ def logout():
     session.pop('username', None)
     return redirect('/loginpage')
 
-# add video in the database/////////////////////////////////////////
-
-
-# add file in upload file
+# upload videos in appropriate folders/////////////////////////////////////////
 class uploadfileform(FlaskForm):
     title = StringField(label=("Description"))
     file = FileField("File", validators=[InputRequired()])
     Dropdown = SelectField('Dropdown',
-                           choices=[('option1', 'Pregnancy'), ('option2', 'Nutrition'), ('option3', 'New Mother')])
+                           choices=[('option1', 'Pregnancy'), ('option2', 'Nutrition'), ('option3', 'New Mother'),
+                            ('option4', 'HIV'), ('option5', 'Dangers in EarlyChildhood'), ('option6', 'TB'), 
+                            ('option7', 'Eat well and save money'), ('option8', 'Immunization')])
     submit = SubmitField("Upload File")
 
     # @app.route('/', methods=['GET', "POST"])
@@ -101,14 +115,24 @@ class uploadfileform(FlaskForm):
             selected_option = form.Dropdown.data
             # Determine the path where the file should be saved based on the selected option
             if selected_option == 'option1':
-                # file_path = '/static/videos/Isifo sephepha (TB)'
-                app.config['UPLOAD_FOLDER'] = 'static/videos/Isifo_sephepha (TB)'
+                # file_path = '/static/videos/pregnancy'
+                app.config['UPLOAD_FOLDER'] = 'static/videos/pregnancy'
             elif selected_option == 'option2':
                 # file_path = '/static/videos/Nutrition'
-                app.config['UPLOAD_FOLDER'] = 'static/videos/Nutrition'
+                app.config['UPLOAD_FOLDER'] = 'static/videos/nutrition'
             elif selected_option == 'option3':
-                # file_path = '/static/videos/Early Childhood'
-                app.config['UPLOAD_FOLDER'] = 'static/videos/Early_Childhood'
+                # file_path = '/static/videos/new mother'
+                app.config['UPLOAD_FOLDER'] = 'static/videos/newmothers'
+            elif selected_option == 'option4':
+                app.config['UPLOAD_FOLDER'] = 'static/videos/hiv'
+            elif selected_option == 'option5':
+                app.config['UPLOAD_FOLDER'] = 'static/videos/earlychildhoodproblems'
+            elif selected_option == 'option6':
+                app.config['UPLOAD_FOLDER'] = 'static/videos/tb'
+            elif selected_option == 'option7':
+                app.config['UPLOAD_FOLDER'] = 'static/videos/eatwellandsavemoney'
+            elif selected_option == 'option8':
+                app.config['UPLOAD_FOLDER'] = 'static/videos/immunization'
             # file.save(file_path)
             file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),
                 app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))  # Then save the file
@@ -176,7 +200,6 @@ create_account('user2', 'password2')
 print(login('user1', 'password1'))  # True
 print(login('user1', 'wrong_password'))  # False
 print(login('wrong_username', 'password1'))  # False """
-
 
 if __name__ == '__main__':
     app.run()
